893999cbf69552420540c9dd4f14307cb9346bc9
Arnold Dziudzik <arn.dziudzik@gmail.com>
2020-11-13 22:19:31 +0200

    progress: bare-bones of the homepage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('This is the Restaurant Page!');
+import loadPage from './modules/initial-page-load';
+import loadHome from './modules/home';
+import loadMenu from './modules/menu';
+import loadContact from './modules/contact';
+
+
+init();
+
+function init() {
+    loadPage();
+    addNavBarEvents();
+}
+function addNavBarEvents() {
+    const homeBtn = document.getElementById('home');
+    const menuBtn = document.getElementById('menu');
+    const contactBtn = document.getElementById('contact');
+
+    homeBtn.addEventListener('click', loadHome);
+    menuBtn.addEventListener('click', loadMenu);
+    contactBtn.addEventListener('click', loadContact);
+}
---
+++ src/modules/initial-page-load.js
@@ -0,0 +1,56 @@
+const { LibManifestPlugin } = require("webpack");
+
+function createHeader(id, text) {
+    const header = document.createElement('header');
+    header.setAttribute('id', id);
+    const logo = document.createElement('h1');
+    logo.textContent = text;
+    header.appendChild(logo);
+    return header;
+}
+function createNavBar(id) {
+    const navBar = document.createElement('nav');
+    navBar.setAttribute('id', id);
+    const homeBtn = createBtn('home', 'home');
+    const menuBtn = createBtn('menu', 'menu');
+    const contactBtn = createBtn('contact', 'contact');
+    navBar.appendChild(homeBtn);
+    navBar.appendChild(menuBtn);
+    navBar.appendChild(contactBtn);
+    return navBar;
+}
+function createBtn(id, text) {
+    const btn = document.createElement('button');
+    btn.setAttribute('id', id);
+    const span = document.createElement('span');
+    span.textContent = text;
+    btn.appendChild(span);
+    btn.classList.add('navbar-tab');
+    return btn;
+}
+function createMain(id) {
+    const main = document.createElement('main');
+    main.setAttribute('id', id);
+    return main;
+}
+function createFooter(id, text) {
+    const footer = document.createElement('footer');
+    footer.setAttribute('id', id);
+    const h3 = document.createElement('h3');
+    h3.textContent = text;
+    footer.appendChild(h3);
+    return footer;
+}
+function loadPage() {
+    const content = document.getElementById('content');
+    const header = createHeader('header', 'Restaurant');
+    content.appendChild(header);
+    const navBar = createNavBar('navbar');
+    content.appendChild(nav);
+    const main = createMain('main');
+    content.appendChild(main);
+    const footer = createFooter('footer', 'Footer');
+    content.appendChild(footer);
+}
+
+export default loadPage;
