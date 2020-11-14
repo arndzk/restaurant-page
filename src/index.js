2c459fe5aab8ac0364ca1ea6aae81444200897b9
Arnold Dziudzik <arn.dziudzik@gmail.com>
2020-11-14 16:09:57 +0200

    progress: bare-bones of contact page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import loadPage from './modules/initial-page-load';
 import loadHome from './modules/home';
 //import loadMenu from './modules/menu';
-//import loadContact from './modules/contact';
+import loadContact from './modules/contact';
 
 console.log('This is a Restaurant Page!');
 
@@ -15,9 +15,9 @@ function init() {
 function addNavBarEvents() {
     const homeBtn = document.getElementById('home');
     //const menuBtn = document.getElementById('menu');
-    //const contactBtn = document.getElementById('contact');
+    const contactBtn = document.getElementById('contact');
 
     homeBtn.addEventListener('click', loadHome);
     //menuBtn.addEventListener('click', loadMenu);
-    //contactBtn.addEventListener('click', loadContact);
+    contactBtn.addEventListener('click', loadContact);
 }
--- src/modules/contact.js
+++ src/modules/contact.js
@@ -1,5 +1,34 @@
+function createSection(sectionTitle, data) {
+    const section = document.createElement('section');
+    section.classList.add('section');
+    const title = document.createElement('h2');
+    title.classList.add('section-title');
+    title.textContent = sectionTitle;
+    section.appendChild(title);
+    data.forEach((d) => {
+        const paragraph = document.createElement('p');
+        paragraph.classList.add('section-contents');
+        paragraph.textContent = d;
+        section.appendChild(paragraph);
+    });
+    return section;
+}
+function setBtnActive(id) {
+    const activeBtn = document.querySelector('.tab.active');
+    if (activeBtn) {
+        activeBtn.classList.remove('active');
+    }
+    const contactBtn = document.getElementById(id);
+    contactBtn.classList.add('active');
+}
 function loadContact() {
-
+    const content = document.getElementById('main');
+    content.textContent = '';
+    const addressSection = createSection('Address', ['Street Name & Number','Postal Code', 'City']);
+    const telephoneSection = createSection('Telephone', ['Telephone Number']);
+    setBtnActive('contact');
+    content.appendChild(addressSection);
+    content.appendChild(telephoneSection);
 }
 
 export default loadContact;
