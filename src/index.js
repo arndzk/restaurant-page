0c04338fd6ed72380175ac44496780ead5b24c65
Arnold Dziudzik <arn.dziudzik@gmail.com>
2020-11-14 18:20:48 +0200

    progress: bare-bones of menu page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import loadPage from './modules/initial-page-load';
 import loadHome from './modules/home';
-//import loadMenu from './modules/menu';
+import loadMenu from './modules/menu';
 import loadContact from './modules/contact';
 
 console.log('This is a Restaurant Page!');
@@ -14,10 +14,10 @@ function init() {
 }
 function addNavBarEvents() {
     const homeBtn = document.getElementById('home');
-    //const menuBtn = document.getElementById('menu');
+    const menuBtn = document.getElementById('menu');
     const contactBtn = document.getElementById('contact');
 
     homeBtn.addEventListener('click', loadHome);
-    //menuBtn.addEventListener('click', loadMenu);
+    menuBtn.addEventListener('click', loadMenu);
     contactBtn.addEventListener('click', loadContact);
 }
--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,7 +1,37 @@
 import setBtnActive from '../utils/active-button-setter'
 
-function loadMenu() {
+function createMenuItem(text, price) {
+    const menuItem = document.createElement('div');
+    menuItem.classList.add('menu-item');
+    const menuItemContents = document.createElement('div');
+    menuItemContents.classList.add('menu-item-contents');
+    const itemText = document.createElement('h3');
+    itemText.textContent = text;
+    const itemPrice = document.createElement('h4');
+    itemPrice.textContent = price;
+    menuItemContents.appendChild(itemText);
+    menuItemContents.appendChild(itemPrice);
+    menuItem.appendChild(menuItemContents);
+    return menuItem;
+}
 
+function loadMenu() {
+    const content = document.getElementById('main');
+    content.textContent = '';
+    setBtnActive('menu');
+    const menuItems = [
+        createMenuItem('item 1', 'price 1'),
+        createMenuItem('item 2', 'price 2'),
+        createMenuItem('item 3', 'price 3'),
+        createMenuItem('item 4', 'price 4'),
+        createMenuItem('item 5', 'price 5'),
+        createMenuItem('item 6', 'price 6'),
+        createMenuItem('item 7', 'price 7'),
+        createMenuItem('item 8', 'price 8')
+    ];
+    menuItems.forEach((menuItem) => {
+        content.appendChild(menuItem);
+    });
 }
 
 export default loadMenu;
